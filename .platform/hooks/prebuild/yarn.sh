eb1293c409efae72a9b48da579024f14f37d5087
Panda Xu <panda@fun.xyz>
2023-05-03 15:10:07 -0700

    Update yarn.sh

--- .platform/hooks/prebuild/yarn.sh
+++ .platform/hooks/prebuild/yarn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # need to install node first to be able to install yarn (as at prebuild no node is present yet)
-sudo curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-sudo yum -y install nodejs
+curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -
+yum -y install nodejs
 # install yarn
-sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-sudo yum -y install yarn
-sudo yum -y install jq
+wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+yum -y install yarn
+yum -y install jq

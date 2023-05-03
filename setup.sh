d2ad632d04632ba43e5ca9a1b40aea34631593e8
Panda Xu <panda@fun.xyz>
2023-05-03 01:24:58 -0700

    setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ yarn && yarn preprocess
 cd localfork && ./setup.sh
 
 # Read entryPointAddress from forkConfig.json
-entryPointAddress=$(jq -r '.entryPointAddress' ./localfork/forkConfig.json)
+cd .. && entryPointAddress=$(jq -r '.entryPointAddress' ./localfork/forkConfig.json)
 
 echo run bundler --network "http://fun-alchemy-fork-eb-2-dev.us-west-2.elasticbeanstalk.com" --entryPoint "${entryPointAddress}" --unsafe
 yarn run bundler --network "http://fun-alchemy-fork-eb-2-dev.us-west-2.elasticbeanstalk.com" --entryPoint "${entryPointAddress}" --unsafe

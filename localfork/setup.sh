ffcdff95ac20d9ebcebc326bfd63d4c58a66b693
Panda Xu <panda@fun.xyz>
2023-05-12 18:04:20 -0700

    config changes

--- localfork/setup.sh
+++ localfork/setup.sh
@@ -1,7 +1,7 @@
 yarn install
 # node ForkSetup.js -l
 node ForkSetup.js -d
-# node ForkSetup.js -dp
+node ForkSetup.js -dp
 # SCRIPT=`node ForkSetup.js -b`
 # echo $SCRIPT
 # cd .. && eval $SCRIPT
--- setup.sh
+++ setup.sh
@@ -7,10 +7,12 @@ yarn && yarn preprocess
 cd localfork && ./setup.sh
 
 # Read entryPointAddress from forkConfig.json
-cd .. && entryPointAddress=$(jq -r '.entryPointAddress' ./localfork/forkConfig.json)
+cd ..
+entryPointAddress=$(jq -r '.entryPointAddress' ./localfork/forkConfig.json)
 
 # Set the desired port number
 port=3000
 
 echo "Running bundler with entryPoint: ${entryPointAddress} on port ${port}"
 yarn run bundler --network "http://fun-alchemy-fork-eb-2-dev.us-west-2.elasticbeanstalk.com" --entryPoint "${entryPointAddress}" --port "${port}" --unsafe 2>&1 | tee bundler.log &
+# yarn run bundler --network "http://fun-alchemy-fork-eb-2-dev.us-west-2.elasticbeanstalk.com" --entryPoint "0xC7B0261c8e65b1d5fFA2eacd5529E583609c1EdF" --port "3000" --unsafe
